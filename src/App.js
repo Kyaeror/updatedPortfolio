58374f7fc6b9fca423a5a6aa5e8465a28bca1393
Brian Chiang <kyaerorgabootcamp@gmail.com>
2023-04-02 20:28:03 -0400

    added contact and footer

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import Header from './components/Header'
 import About from './components/About'
 import Project from './components/Project'
 import Resume from './components/Resume'
-
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <Router>
         <Header />
-        
+        <Footer />
         <Routes>
           <Route path='/' element={<About />}/>
           <Route path='/project' element={<Project />}/>
           <Route path='/resume' element={<Resume />}/>
-          
+          <Route path='/contact' element={<Contact />}/>
         </Routes>
     </Router>
   )
